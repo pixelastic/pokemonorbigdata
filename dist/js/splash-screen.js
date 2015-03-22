1e28dc21322b3a999f961fbce5a61e134a2ac2dd
Pixelastic <tim@pixelastic.com>
2015-03-22 22:22:31 +0100

    Building for gh-pages

---
+++ dist/js/splash-screen.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var SplashScreen = React.createClass({displayName: "SplashScreen",
+  render: function() {
+    return (
+      React.createElement("div", {className: "splashscreen"}, "Is it big data or Pokemon ?")
+    )
+  }
+});
+
