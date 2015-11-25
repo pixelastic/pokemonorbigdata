aa6d696be0e4b1c078b04a2da0e41a698342038f
Pixelastic <tim@pixelastic.com>
2015-11-25 19:55:23 +0100

    Building for gh-pages

--- js/answer.js
+++ js/answer.js
@@ -9,12 +9,15 @@ var Answer = React.createClass({displayName: "Answer",
     });
     var img = "./img/" + this.props.question.img;
     var name = this.props.question.name;
+    var url = this.props.question.url;
     var text = this.props.question.text;
     var type = this.props.question.type === IS_BIGDATA ? 'Big Data' : 'Pokemon';
 
     return (
       React.createElement("div", {className: classes}, 
-        React.createElement("h1", {className: "answer-name"}, name, " is ", type, "!"), 
+        React.createElement("h1", {className: "answer-name"}, 
+            React.createElement("a", {href: url, target: "_blank"}, name), " is ", type, "!"
+        ), 
         React.createElement("div", {className: "answer-picture"}, React.createElement("img", {src: img, alt: name})), 
         React.createElement("div", {className: "answer-text"}, text), 
         React.createElement("div", {className: "answer-next"}, 
