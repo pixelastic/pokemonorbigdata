9c2933a8841742eefbdc0ebc205ff6bc202edaa0
Tim Carry <tim@pixelastic.com>
2021-04-11 20:48:47 +0200

    Replace Pokemon with Pokémon

--- app/js/answer.jsx
+++ app/js/answer.jsx
@@ -11,7 +11,7 @@ var Answer = React.createClass({
     var name = this.props.question.name;
     var url = this.props.question.url;
     var text = this.props.question.text;
-    var type = this.props.question.type === IS_BIGDATA ? 'Big Data' : 'Pokemon';
+    var type = this.props.question.type === IS_BIGDATA ? 'Big Data' : 'Pokémon';
 
     return (
       <div className={classes}>
--- app/js/splash-screen.jsx
+++ app/js/splash-screen.jsx
@@ -3,7 +3,7 @@
 var SplashScreen = React.createClass({
   render: function() {
     return (
-      <div className="splashscreen">Is it big data or Pokemon ?</div>
+      <div className="splashscreen">Is it Pokemon or Big Data?</div>
     )
   }
 });
