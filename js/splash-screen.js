b98a5ade9d209dbe09835d8c7384d2c7c978f22e
Tim Carry <tim@pixelastic.com>
2021-04-11 20:58:49 +0200

    Updates

--- js/splash-screen.js
+++ js/splash-screen.js
@@ -3,7 +3,7 @@
 var SplashScreen = React.createClass({displayName: "SplashScreen",
   render: function() {
     return (
-      React.createElement("div", {className: "splashscreen"}, "Is it big data or Pokemon ?")
+      React.createElement("div", {className: "splashscreen"}, "Is it Pokemon or Big Data?")
     )
   }
 });
